a16be1a37f6d86e603f5c8a791d8549088c6b056
Antonio Victor <Antoniovictor12@live.com>
2022-10-05 19:35:29 -0300

    implementacao classe form

--- src/app.ts
+++ src/app.ts
@@ -1,89 +1,50 @@
 import "./styles.css";
 
-import { CepList } from "./classes/cep-list.js";
-import { Api } from "./api/api.js";
-import { cepStorage } from "./store/app-storage.js";
-import { controlInput } from "./utils/control-input.js";
+import { controlInput } from "./utils/control-input";
 
-// intances
-const ceps = new CepList();
-const api = new Api();
+abstract class ControledInput {
+  formElement: HTMLFormElement;
+  inputElement: HTMLInputElement;
 
-// elements selection
-const header_inputText = document.getElementById(
-  "main-header__input-cep"
-) as HTMLInputElement;
-
-const header_form = document.querySelector(
-  ".main-header__form"
-) as HTMLFormElement;
-
-const actions_inputText = document.getElementById(
-  "main-content__actions-input"
-) as HTMLFormElement;
-
-const actions_form = document.querySelector(
-  ".main-content__actions-form"
-) as HTMLFormElement;
-
-// handlers
-async function header_form_submitHandler(e: SubmitEvent) {
-  e.preventDefault();
-
-  if (header_inputText.value.length !== 8) {
-    alert("Insira um cep válido! Um cep deve conter 8 digitos");
-    return;
-  }
-
-  if (
-    cepStorage.localItems.find((item) => item.cep === header_inputText.value)
+  constructor(
+    formSelector: string,
+    inputSelector: string,
+    inputControlFn: (e: Event) => void
   ) {
-    ceps.goToCep(header_inputText.value);
-    ceps.focusCep(header_inputText.value);
-    alert("Cep já pesquisado, verifique na lista");
-    return;
+    this.formElement = document.querySelector(formSelector) as HTMLFormElement;
+    this.inputElement = document.querySelector(
+      inputSelector
+    ) as HTMLInputElement;
+
+    this.formElement.addEventListener("submit", this.submit.bind(this));
+    this.inputElement.addEventListener("input", inputControlFn);
   }
 
-  const newCep = await api.findCep(header_inputText.value);
+  abstract submit(event: SubmitEvent): void;
 
-  if (!newCep) {
-    alert("Algo deu errado... tente novamente.");
-    return;
+  protected resetValues() {
+    this.inputElement.value = "";
   }
-
-  ceps.addNewCep(newCep);
-  ceps.goToCep(newCep.cep);
-  ceps.focusCep(newCep.cep);
-  header_inputText.value = "";
 }
 
-function action_form_submitHandler(e: SubmitEvent) {
-  e.preventDefault();
-
-  if (actions_inputText.value.length !== 8) {
-    alert("Insira um cep válido! Um cep deve conter 8 digitos");
-    return;
-  }
-
-  if (
-    !cepStorage.localItems.find((item) => item.cep === actions_inputText.value)
+class HeaderForm extends ControledInput {
+  constructor(
+    formSelector: string,
+    inputSelector: string,
+    inputControlFn: (e: Event) => void
   ) {
-    alert("Cep não existe na lista, pesquise na barra superior.");
-    return;
+    super(formSelector, inputSelector, inputControlFn);
   }
 
-  ceps.goToCep(actions_inputText.value);
-  ceps.focusCep(actions_inputText.value);
-  actions_inputText.value = "";
+  submit(event: SubmitEvent) {
+    event.preventDefault();
+    console.log("teste");
+    this.resetValues();
+  }
 }
 
-// events
-header_inputText.addEventListener("input", controlInput);
-header_form.addEventListener("submit", header_form_submitHandler);
-
-(
-  document.getElementById("main-content__deleteAll-btn") as HTMLButtonElement
-).addEventListener("click", () => ceps.resetCeps());
-
-actions_inputText.addEventListener("input", controlInput);
-actions_form.addEventListener("submit", action_form_submitHandler);
+const headerForm = new HeaderForm(
+  ".main-header__form",
+  "#main-header__input-cep",
+  controlInput
+);
