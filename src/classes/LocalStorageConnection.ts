c72f274d29f7d264fa2fb52128629c90a3fd9a7b
Antonio Victor <Antoniovictor12@live.com>
2022-10-06 16:16:59 -0300

    implementando classe localStorage

---
+++ src/classes/LocalStorageConnection.ts
@@ -0,0 +1,24 @@
+// criar uma nova classe para o state?
+// essa nova classe recebe
+
+import { IUpdatable } from "../models/updatable";
+
+export class LocalStorageConnection<T> implements IUpdatable {
+  constructor(private localStorageName: string) {
+    if (!localStorage.getItem(this.localStorageName)) {
+      localStorage.setItem(this.localStorageName, JSON.stringify([]));
+    }
+  }
+
+  get items(): T[] {
+    return JSON.parse(localStorage.getItem(this.localStorageName)!);
+  }
+
+  update<T>(items: T[]) {
+    localStorage.setItem(this.localStorageName, JSON.stringify(items));
+  }
+
+  reset() {
+    localStorage.setItem(this.localStorageName, JSON.stringify([]));
+  }
+}
